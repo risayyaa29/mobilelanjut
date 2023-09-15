73b36f7eb6e273a4229ceee60c6a7137fdc29f77
risayyaa29 <106370248+risayyaa29@users.noreply.github.com>
2023-09-15 10:14:47 +0700

    Delete recipeapp directory

--- recipeapp/lib/main.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:recipeapp/app.dart';
-import 'package:recipeapp/viewmodels/recipe_viewmodel.dart';
-
-void main() async {
-  await RecipeViewModel.load();
-  runApp(RecipeApp());
-}
